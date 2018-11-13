f27fb3893dec540f1b2f35de8535822e1e81a6a2
Kevin <defonix0@gmail.com>
2018-11-12 21:04:59 -0500

    routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from "@angular/core";
-import { NativeScriptRouterModule } from "nativescript-angular/router";
-import { Routes } from "@angular/router";
-import { AppComponent } from "./app.component";
-
+import { NgModule } from '@angular/core';
+import { NativeScriptRouterModule } from 'nativescript-angular/router';
+import { Routes } from '@angular/router';
 
 const routes: Routes = [
-    { path: "", redirectTo: "/home", pathMatch: "full" },
-    { path: "home", component: AppComponent },
+    {
+        path: '',
+        redirectTo: '/mixer/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'mixer',
+        loadChildren: () => require('./modules/mixer/mixer.module')['MixerModule']
+    },
+    {
+        path: 'record',
+        loadChildren: () => require('./modules/recorder/recorder.module')['RecorderModule']
+    }
+        
 ];
 
 @NgModule({
-    imports: [NativeScriptRouterModule.forRoot(routes)],
-    exports: [NativeScriptRouterModule]
-})
+    imports: [
+    NativeScriptRouterModule.forRoot(routes)
+    ],
+    exports: [
+    NativeScriptRouterModule
+    ]
+})   
 export class AppRoutingModule { }
